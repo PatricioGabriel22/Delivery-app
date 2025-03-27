72f0b2f2c65b104f763b4aa763718871fcfb62d7
PatricioGabriel22 <ppucheta.dev@gmail.com>
2025-03-27 11:35:55 -0300

    cambios en barra de nav

--- delivery-app/src/components/Nav.jsx
+++ delivery-app/src/components/Nav.jsx
@@ -5,13 +5,10 @@ import { CiShoppingCart } from "react-icons/ci";
 export default function Nav(){
     return (
         <div className="flex flex-col justify-center items-center mt-auto min-w-screen">
-
-            {/* Círculo incrustado en la barra roja */}
-            <div className="rounded rounded-b-2xl bg-cyan-600 h-20 w-20 absolute bottom-3 flex items-center justify-center">
-                <CiShoppingCart size={40} className="text-white "/>
-            </div>
           
-    
+            <CiShoppingCart size={90} className="text-black bg-white rounded-full fixed bottom-5 flex flex-col items-center"/>
+            <p className="text-black text-6xl absoltue">10</p>
+             
             <div className="flex flex-row bg-red-600 justify-between md:justify-evenly p-4 rounded-t-4xl w-full">
                 <h1>Profile</h1>
                 <LogoutBTN />
