14de35c60867c7a2a1e34f88854cf58cc5c6544e
PatricioGabriel22 <ppucheta.dev@gmail.com>
2025-03-27 11:41:03 -0300

    nav 3

--- delivery-app/src/components/Nav.jsx
+++ delivery-app/src/components/Nav.jsx
@@ -6,11 +6,12 @@ export default function Nav(){
     return (
         <div className="flex flex-col justify-center items-center mt-auto min-w-screen ">
           
-            <CiShoppingCart size={90} className="text-black bg-white rounded-full absolute bottom-5 "/>
-            <p className="text-black text-6xl ">10</p>
-             
             <div className="flex flex-row bg-red-600 justify-between md:justify-evenly p-4 rounded-t-4xl w-full">
                 <h1>Profile</h1>
+                <div className="text-black bg-white rounded-3xl absolute bottom-1 flex flex-col items-center justify-center">
+                    <CiShoppingCart size={90} />
+                    <p className="text-black ">1</p>
+                </div> 
                 <LogoutBTN />
             </div>
 
