ea8bd5a3ecf12bd1b04d60838ec06b32e8334d4f
PatricioGabriel22 <ppucheta.dev@gmail.com>
2025-03-27 11:37:33 -0300

    cambios en barra de nav 2

--- delivery-app/src/components/Nav.jsx
+++ delivery-app/src/components/Nav.jsx
@@ -4,10 +4,10 @@ import { CiShoppingCart } from "react-icons/ci";
 
 export default function Nav(){
     return (
-        <div className="flex flex-col justify-center items-center mt-auto min-w-screen">
+        <div className="flex flex-col justify-center items-center mt-auto min-w-screen ">
           
-            <CiShoppingCart size={90} className="text-black bg-white rounded-full fixed bottom-5 flex flex-col items-center"/>
-            <p className="text-black text-6xl absoltue">10</p>
+            <CiShoppingCart size={90} className="text-black bg-white rounded-full absolute bottom-5 "/>
+            <p className="text-black text-6xl ">10</p>
              
             <div className="flex flex-row bg-red-600 justify-between md:justify-evenly p-4 rounded-t-4xl w-full">
                 <h1>Profile</h1>
