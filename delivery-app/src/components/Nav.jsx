4fefc76f6f8c262edc8397477b8d8ba0c09ac7e1
PatricioGabriel22 <ppucheta.dev@gmail.com>
2025-03-27 11:51:35 -0300

    nav4

--- delivery-app/src/components/Nav.jsx
+++ delivery-app/src/components/Nav.jsx
@@ -4,14 +4,18 @@ import { CiShoppingCart } from "react-icons/ci";
 
 export default function Nav(){
     return (
-        <div className="flex flex-col justify-center items-center mt-auto min-w-screen ">
+        <div className="mt-auto min-w-screen ">
           
-            <div className="flex flex-row bg-red-600 justify-between md:justify-evenly p-4 rounded-t-4xl w-full">
+
+            <div className="flex flex-row bg-red-600 justify-evenly items-center md:justify-evenly rounded-t-4xl w-full p-4 relative">
+
                 <h1>Profile</h1>
-                <div className="text-black bg-white rounded-3xl absolute bottom-1 flex flex-col items-center justify-center">
-                    <CiShoppingCart size={90} />
-                    <p className="text-black ">1</p>
+
+                <div className="text-black bg-white rounded-3xl  flex flex-col items-center justify-center absolute bottom-6">
+                    <CiShoppingCart size={60} />
+                    <p className="text-black ">10</p>
                 </div> 
+
                 <LogoutBTN />
             </div>
 
