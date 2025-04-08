7906b4b88342f7312f49e8969db82a2ac5297688
PatricioGabriel22 <ppucheta.dev@gmail.com>
2025-04-07 21:21:02 -0300

    areglando endpoint

--- delivery-app/src/pages/CarritoConfirm.jsx
+++ delivery-app/src/pages/CarritoConfirm.jsx
@@ -64,7 +64,7 @@ export default function CarritoConfirm(){
             importeTotal:importeTotal
         }
 
-        axios.post(`${renderORLocalURL}/pedidos/sendPreOrder`,payload,{withCredentials:true})
+        axios.post(`${renderORLocalURL}/sendPreOrder`,payload,{withCredentials:true})
 
     }
 
