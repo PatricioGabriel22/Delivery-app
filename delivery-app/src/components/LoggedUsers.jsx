b54a38c8aa04767e8695929974c584e409df8560
PatricioGabriel22 <ppucheta.dev@gmail.com>
2025-04-27 14:22:56 -0300

    agregue al front para ver usuarios en linea y lo configure para sm y md+ parte 2

--- delivery-app/src/components/LoggedUsers.jsx
+++ delivery-app/src/components/LoggedUsers.jsx
@@ -29,10 +29,10 @@ export default function LoggedUsers(){
                 <h3 className="text-center font-extrabold text-xl p-1 mb-2">Usuarios en la tienda</h3>
 
 
-                <div  className="flex flex-col  items-center px-1 ">
+                <div  className="flex flex-col  items-center ">
                     {loggedUsers.length >0 && loggedUsers?.map((user,index)=>(
-                        <div className="flex flex-row w-full items-center rounded h-9 md:h-12 border-1 my-1 hover:bg-white cursor-pointer" key={index}>
-                            <span className="bg-green-600 rounded-full w-4 h-4 p-2 mx-1"/>
+                        <div className=" bg-green-200 flex flex-row w-full items-center rounded h-9 md:h-12 border-1 my-1 hover:bg-white cursor-pointer" key={index}>
+                            <span className="bg-green-600 rounded-full w-4 h-4 p-2 mx-1 border-2"/>
                             <p className="w-2/3">{ccapitalizer_3000(user.username)}</p>
                             <p className="w-2/3 text-end pr-2">{ccapitalizer_3000(user.localidad)}</p>
                         </div>
