063d57deef417d2e5ae0e5c702b71ecc92116ccb
PatricioGabriel22 <ppucheta.dev@gmail.com>
2025-05-05 18:44:01 -0300

    instalando dependecia cloudinary

--- backend/src/cloudinary.js
+++ backend/src/cloudinary.js
@@ -1,6 +1,6 @@
-import { v2 as cloudinary } from 'cloudinary'
-import { CloudinaryStorage } from 'multer-storage-cloudinary'
-import dotenv from 'dotenv'
+import { v2 as cloudinary } from 'cloudinary.js'
+import { CloudinaryStorage } from 'multer-storage-cloudinary.js'
+import dotenv from 'dotenv.js'
 
 
 dotenv.config({
