aab6e04e0c5530e05cd5e405e63f7a868dfe7e5b
PatricioGabriel22 <ppucheta.dev@gmail.com>
2025-05-17 16:32:19 -0300

    arreglando registro

--- delivery-app/src/pages/Register.jsx
+++ delivery-app/src/pages/Register.jsx
@@ -29,9 +29,9 @@ export default function Register(){
         const registerData = {
             username:e.target[0].value.trim(),
             password:e.target[1].value,
-            direccion:e.target[3].value.toLowerCase(),
-            localidad:e.target[4].value.toLowerCase(),
-            entreCalles:e.target[5].value.toLowerCase(),
+            direccion:e.target[3].value,
+            localidad:e.target[4].value,
+            entreCalles:e.target[5].value,
             telefono:e.target[6].value
         }
 
