93fdf6c5fbea7c473c4beebb026e8ecd7ebe297f
PatricioGabriel22 <ppucheta.dev@gmail.com>
2025-05-20 13:12:07 -0300

    agregando pwa

--- delivery-app/vite.config.js
+++ delivery-app/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -12,7 +14,30 @@ const __dirname = path.dirname(__filename)
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Delivery App',
+        short_name: 'Delivery',
+        start_url: '/',
+        display_override: ['window-controls-overlay', 'standalone', 'browser'],
+        background_color: '#000000',
+        theme_color: '#FF0000',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
   ],
   resolve:{
     alias:{
