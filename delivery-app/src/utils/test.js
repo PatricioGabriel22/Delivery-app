16d0a0d131dfc9de36bf1050358fbbfeec09befd
PatricioGabriel22 <ppucheta.dev@gmail.com>
2025-07-23 19:45:41 -0300

    .

--- delivery-app/src/utils/test.js
+++ delivery-app/src/utils/test.js
@@ -1,22 +1,23 @@
-import Fuse from 'fuse.js'
+// import Fuse from 'fuse.js'
 
-const zonas_delivery = [
-    {
-        zona:"monte grande",
-        costoEnvio: 3500
+// const zonas_delivery = [
+//     {
+//         zona:"monte grande",
+//         costoEnvio: 3500
 
-    },
-    {
-      zona:"luis guillon",
-      costoEnvio:2500  
-    }
-]
+//     },
+//     {
+//       zona:"luis guillon",
+//       costoEnvio:2500  
+//     }
+// ]
 
-const fuse = new Fuse(zonas_delivery,{threshold: 0.3, keys:["zona"]})
+// const fuse = new Fuse(zonas_delivery,{threshold: 0.3, keys:["zona"]})
 
-const result = fuse.search("mont3")
+// const result = fuse.search("mont3")
 
-console.log(result)
+// console.log(result)
 
 
-
+const datePedido = new Date('2025-07-21T12:03:43.205+00:00').toLocaleDateString('es-AR')
+console.log( datePedido === new Date().toLocaleDateString('es-AR'))
